1ef00d7ae1d125dac929c3fcb95d8787e3e4f47a
yashgarg7302 <yashgarg7302@gmail.com>
2025-11-24 12:16:22 +0530

    change in project ordering

--- data/projects.js
+++ data/projects.js
@@ -1,11 +1,5 @@
 const projects = [
-  {
-  title: "Spam Email Classifier (C++ · ML)",
-  description:
-  "Naïve Bayes classifier trained on 4.6k+ UCI Spambase emails. Implemented log-probabilities; evaluated with precision/recall/F1. Benchmarked vs Weka (80%). Achieved 78.3% accuracy.",
-  link: "https://github.com/yashgarg7302/Email-Spam-Classifier",
-  tags: ["C++", "Naïve Bayes", "ML"],
-  },
+  
   {
     title: "PDF Files Merger Web App",
     description:
@@ -14,26 +8,34 @@ const projects = [
   tags: ["Node.js", "Express", "Multer", "PDF-Lib", "File Handling"],
   },
   {
-  title: "Lane Detection for Self‑Driving Cars",
-  description:
-  "OpenCV pipeline using Canny + Hough Transform with ROI masking and slope filtering for robust lane boundary detection.",
-  link: "https://github.com/yashgarg7302/Lane_Detection",
-  tags: ["Python", "OpenCV", "Computer Vision"],
-  },
-  {
-  title: "CPU Scheduling Visualizer",
-  description:
-  "Interactive visualizations for FCFS, SJF, Round Robin, and Priority with dynamic Gantt charts and metrics.",
-  link: "https://scheduling-algo-visualizer.netlify.app/",
-  tags: ["JavaScript", "HTML", "CSS"],
-  },
-  {
     title: "CodeCast – Real-time Code Collaboration",
     description:
       "A real-time collaborative code editor built using the MERN stack (MongoDB, Express.js, React, Node.js) with Socket.IO. Multiple users can join a shared room, edit code simultaneously, and see live updates with syntax highlighting.",
     link: "https://code-collab-lemon.vercel.app/",
     tags: ["MERN", "Socket.IO", "React", "Node.js", "Real-time"],
   },
+  {
+    title: "CPU Scheduling Visualizer",
+    description:
+    "Interactive visualizations for FCFS, SJF, Round Robin, and Priority with dynamic Gantt charts and metrics.",
+    link: "https://scheduling-algo-visualizer.netlify.app/",
+    tags: ["JavaScript", "HTML", "CSS"],
+    },
+  {
+    title: "Spam Email Classifier (C++ · ML)",
+    description:
+    "Naïve Bayes classifier trained on 4.6k+ UCI Spambase emails. Implemented log-probabilities; evaluated with precision/recall/F1. Benchmarked vs Weka (80%). Achieved 78.3% accuracy.",
+    link: "https://github.com/yashgarg7302/Email-Spam-Classifier",
+    tags: ["C++", "Naïve Bayes", "ML"],
+    },
+  {
+  title: "Lane Detection for Self‑Driving Cars",
+  description:
+  "OpenCV pipeline using Canny + Hough Transform with ROI masking and slope filtering for robust lane boundary detection.",
+  link: "https://github.com/yashgarg7302/Lane_Detection",
+  tags: ["Python", "OpenCV", "Computer Vision"],
+  },
+  
 
   ];
   export default projects;
